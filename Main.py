dd4586a126374c2db1196f14eae69d4797e28414
Murilo Hunold Martinho <mhunold@practia.global>
2021-07-18 23:59:56 -0300

    changed comments

--- Main.py
+++ Main.py
@@ -6,87 +6,86 @@ from pygame import font
 from random import randint
 import time
 
-os.environ['SDL_VIDEO_CENTERED']='1' #Centraliza a janela na tela
+os.environ['SDL_VIDEO_CENTERED']='1' #Centers the window on the screen
 pygame.init()
 
 #Variables
-bombs=0 # Total Bombas
-acertos=0 #Contador do numero de bombas marcadas com bandeiras
-t=10 #tamanho da matriz quadrada
-fim=False #Define se o jogo terminou ou nao
-fullscreen=False #Define se o jogo está em FS ou nao
+bombs=0 # Total Bombs
+hits=0 #Counter the number of bombs marked with flags
+t=10 #board headquarters size
+endgame=False #End of the game
+fullscreen=False #Defines if the game is in FS or not
 
-#Gera uma matriz para guardar os valores e bombas
+#Generates a headquarters to store values ​​and bombs
 campo = [["" for j in range(t)] for i in range(t)]
-#Matriz que determina se uma posicao foi aberta ou nao pelo usuario
+#Matrix that determines if a position was opened or not by the user
 af = [[0 for j in range(t)] for i in range(t)]
-#Matriz que guarda se a posicao foi marcada com uma bandeira
+#Matrix that keeps if the position was marked with a flag
 mband = [[0 for j in range(t)] for i in range(t)]
 
 
-#Cria um relogio para cronometrar o jogo
+#create a clock to time the game
 relogio = pygame.time.Clock()
 
-#Define a Janela, titulo, cursor e icone
+#Defines Window, Title, Cursor and Icon
 wnd = pygame.display.set_mode( (640,610), 0, 32)
 pygame.display.set_caption("Deviget Minesweeper - By Murilo Hunold")
 pygame.mouse.set_cursor(*pygame.cursors.diamond)
 
-#Cria Surfaces
-cabec = pygame.Surface( (640, 80), 0, 32)
+#Create Surfaces
+header = pygame.Surface((640, 80), 0, 32)
 screen = pygame.Surface( (640, 480), 0, 32)
 pts = pygame.Surface( (640, 50), 0, 32)
 
-#Define comprimento e altura de cada casa do campo minado
+#Defines the length and height of each minefield house
 comp = int(screen.get_width()/t)
 alt = int(screen.get_height()/t)
 
-#Cria fonte para os numeros do campo de acordo com o tamanho da matriz e com fonte padrao
+#Create font for field numbers according to matrix size and default font
 my_font = pygame.font.SysFont("", int(700/t))
 menu_fnt = pygame.font.SysFont("Segoe UI", 16, True, False)
 msg_fnt = pygame.font.SysFont("Verdana", 14, True, False)
 
 #Images
-#Casa padrao
+#Normal square
 q = pygame.transform.scale(pygame.image.load("quad0.png").convert_alpha(), (comp, alt))
-#Casa clicada
+#clicked square
 q2 = pygame.transform.scale(pygame.image.load("quad1.png").convert_alpha(), (comp, alt))
-#Bandeira
-band = pygame.transform.scale(pygame.image.load("band.png").convert_alpha(), (comp, alt))
+#Flag
+flag = pygame.transform.scale(pygame.image.load("band.png").convert_alpha(), (comp, alt))
 #Bomb
 bomba = pygame.transform.scale(pygame.image.load("bomb.png").convert_alpha(), (comp, alt))
 #Logo do Jogo
 #logo = pygame.image.load("logo.png").convert_alpha()
 
 
-#Define o icone da janela
+#Defines the window icon
 pygame.display.set_icon(bomba)
 
-#Funcao que inicia a partida
-def inicio():
-    #Define o uso de variaveis globais
-    global campo, af, mband, t, bombs, acertos, fim, jogou, relogio
-    #Inicializa/zera as variaveis globais
-    bombs=0; acertos=0; fim=False; jogou=False
+#Function that starts the game
+def Start():
+    #Defines the use of global variables
+    global campo, af, mband, t, bombs, hits, endgame, played, relogio
+    #Initializes / resets global variables
+    bombs=0; hits=0; endgame=False; played=False
     campo= [["" for j in range(t)] for i in range(t)]
     af= [[0 for j in range(t)] for i in range(t)]
     mband= [[0 for j in range(t)] for i in range(t)]
 
-    #Desenha o Menu
+    #Design the Menu
     pts.fill( (0,0,0) )
-    #pts.blit(menu_fnt.render("[M] Música [T] Tela cheia [R] Reiniciar [S] Sair", True, (255, 255, 255)), (250, 20))
     pts.blit(menu_fnt.render("[F] Full Screen [R] Restart [Q] Quit", True, (255, 255, 255)), (250, 20))
-    #Desenho o cenário
+    #Design the scenery
     for x in range(t):
         for y in range(t):
             screen.blit(q, (x*comp, y*alt))
         pygame.display.update()
 
 
-#Funcao para distribuir as bombas na matriz
-def Criar(x1, y1):
+#Function to distribute the bombs in the matrix
+def Create(x1, y1):
     global bombs
-    #Adiciona bombas na matriz sorteando uma posicao
+    #Add bombs to the matrix by drawing a position
     for b in range(0, int((t*t)/5)):
         a1=randint(0,t-1)
         a2=randint(0,t-1)
@@ -96,7 +95,7 @@ def Criar(x1, y1):
         else:
             b-=1
     num=0
-    #Conta o numero de bombas em volta de cada posicao de matriz
+    #Count the number of bombs around each matrix position
     for y in range(t):
         for x in range(t):
             if (campo[y][x] == ""):
@@ -109,8 +108,8 @@ def Criar(x1, y1):
                 num=0
     return True
 
-#Funcao que adiciona diferentes cores para os numeros
-def Colorir(v):
+#Function that adds different colors to the numbers
+def Color(v):
     if(v =="1"):
         imgpeca = my_font.render(v, True, (176, 196, 222))
     elif(v == "2"):
@@ -121,27 +120,27 @@ def Colorir(v):
         imgpeca = my_font.render(v, True, (0, 0, 140))
     return imgpeca
 
-#Funcao recursiva para abrir os campos em branco
-def abrir(x, y):
+#Recursive function to open blank fields
+def Open(x, y):
     for l in range((y-1), (y+2)):
         for c in range((x - 1), (x + 2)):
             if ((l>=0 and c>=0) and (l<t and c<t)):
                 if(mband[l][c]==0):
                     screen.blit(q2, (c*comp, l*alt))
                     if(campo[l][c]!="0"):
-                        screen.blit(Colorir(campo[l][c]), ((c*comp+(comp/4)), l*alt))
+                        screen.blit(Color(campo[l][c]), ((c * comp + (comp / 4)), l * alt))
                     elif(campo[l][c]=="0" and (af[l][c]==0 and (l!=y or c!=x))):
-                        abrir(c,l)
+                        Open(c, l)
                     af[l][c]=1
 
-#Funcao para explodir as bombas em espiral
-def Explodir(x,y):
+#Function to explode the bombs
+def Explode(x, y):
     screen.blit(bomba, ((int(x)*comp), (int(y)*alt)))
     wnd.blit(screen, (0,0))
     b=1
     m=0
     n=1
-    global bombs, fim, relogio
+    global bombs, endgame, relogio
     while(b<bombs):
         for l in range(y+n-1, y-n-1, -1):
             if((l>=0 and (x-n)>=0)and (l<t and (x-n)<t)):
@@ -174,71 +173,70 @@ def Explodir(x,y):
                     b += 1
         m+=1
         n+=1
-    fim=True
-    #pts.blit(msg_fnt.render("Que pena, você perdeu! Tempo: "+str(relogio.tick()/1000)+ " seg", True, (230, 230, 0), (10, 10, 10)))
+    endgame=True
     pts.blit(msg_fnt.render("Sorry, you lost! Time: "+str(relogio.tick()/1000)+ " seg", True, (230, 230, 0)),(10, 0))
 
-#Funcao para verificar a jogada
-def Jogar(x, y):
+#Function to check the player move
+def Play(x, y):
     af[y][x]=1
     screen.blit(q2, (x*comp, y*alt))
     if (campo[y][x] !='*'):
         if(campo[y][x]=="0"):
-            abrir(x,y)
+            Open(x, y)
         else:
-            screen.blit(Colorir(campo[y][x]), ((x*comp+(comp/4)), y*alt))
+            screen.blit(Color(campo[y][x]), ((x * comp + (comp / 4)), y * alt))
     else:
-        Explodir(x,y)
+        Explode(x, y)
 
-#Marcar Bandeiras
-def Bandeirar(x,y):
-    global acertos, bombs, fim
+#Mark Flags
+def Flags(x, y):
+    global hits, bombs, endgame
     if(mband[y][x]==0 and af[y][x]==0):
         if(campo[y][x]=="*"):
-            acertos+=1
+            hits+=1
         mband[y][x]=1
-        screen.blit(band, ((int(x)*comp+(comp/8)), (int(y)*alt+(alt/8))))
+        screen.blit(flag, ((int(x) * comp + (comp / 8)), (int(y) * alt + (alt / 8))))
     elif(af[y][x]==0):
         if(campo[y][x]=="*"):
-            acertos-=1
+            hits-=1
         screen.blit(q, ( (int(x))*comp, (int(y))*alt))
         mband[y][x]=0
-    if(acertos==bombs and bombs>0):
-        fim=True
+    if(hits==bombs and bombs>0):
+        endgame=True
         pts.blit(msg_fnt.render("Congratulations, you WON! Time: "+str(relogio.tick()/1000)+" seg", True, (230, 230, 0)),(10, 0))
 pygame.display.update()
-jogou=False
-inicio()
+played=False
+Start()
 
 while True:
     for e in pygame.event.get():
         if e.type == QUIT:
             exit()
-        #Opcoes de clique
-        if ((e.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()==(1,0,0) and fim ==False)):
+        #click options
+        if ((e.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed() == (1,0,0) and endgame == False)):
             (x,y) = e.pos
             if(int(y/alt)<t):
-                if(not jogou):
-                    jogou=Criar(int(x/comp), int(y/alt))
-                    Jogar(int(x/comp), int(y/alt))
-                    #marca inicio da contagem
+                if(not played):
+                    played=Create(int(x / comp), int(y / alt))
+                    Play(int(x / comp), int(y / alt))
+                    #Starts clock
                     relogio.tick()
                 elif(mband[int(y/alt)][int(x/comp)]==0):
-                    Jogar(int(x / comp), int(y / alt))
-        if (((e.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()==(0,0,1)) and fim == False)and jogou==True):
+                    Play(int(x / comp), int(y / alt))
+        if (((e.type == MOUSEBUTTONDOWN and pygame.mouse.get_pressed()==(0,0,1)) and endgame == False)and played==True):
             if(int(y/alt)<t):
                 (x,y) = e.pos
-                Bandeirar(int(x/comp), int(y/alt))
-        wnd.blit(cabec, (0, 480))
+                Flags(int(x / comp), int(y / alt))
+        wnd.blit(header, (0, 480))
         wnd.blit(screen, (0, 0))
         wnd.blit(pts, (0, 560))
         #cabec.blit(logo, (0, 0))
 
         pygame.display.update()
 
-        #Opcoes do teclado
+        #keyboard options
         if(e.type == KEYDOWN and e.key==K_r):
-            inicio()
+            Start()
         if (e.type == KEYDOWN and e.key == K_q):
             exit()
         if (e.type == KEYDOWN and e.key == K_f):
